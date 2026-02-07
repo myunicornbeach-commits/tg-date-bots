444f9e3233f56ede4c358165e1b73be650611d25
myunicornbeach-commits <myunicornbeach@gmail.com>
2026-02-07 23:33:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-import time
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-print("Bot is running")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-while True:
-    time.sleep(60)
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Привет. Я готов к свиданию.\nВыбери, куда пойдём."
+    )
+
+def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    print("Bot started")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
